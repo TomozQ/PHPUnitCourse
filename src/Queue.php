137385592aa6130022049e4c9df6fbfc9d0e68e9
TomozQ <inf.4clover@gmail.com>
2024-01-24 23:22:30 +0900

    queue test

--- src/Queue.php
+++ src/Queue.php
@@ -2,5 +2,20 @@
 
 class Queue
 {
-  
+  protected $items = [];
+
+  public function push($item)
+  {
+    $this->items[] = $item;
+  }
+
+  public function pop()
+  {
+    return array_pop($this->items);
+  }
+
+  public function getCount()
+  {
+    return count($this->items);
+  }
 }
