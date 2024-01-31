c5e13638a8eb582735d4897107e074771d2ae308
TomozQ <inf.4clover@gmail.com>
2024-01-31 23:21:43 +0900

    Dependency Injection

--- src/User.php
+++ src/User.php
@@ -17,6 +17,28 @@ class User
   public $surname;
 
   /**
+   * Email address
+   * @var string
+   */
+  public $email;
+
+  /**
+   * Mailer object
+   * @var Mailer
+   */
+  protected $mailer;
+
+  /**
+   * Set the mailer dependency
+   * 
+   * @param Mailer $mailer The Mailer object
+   */
+  public function setMailer(Mailer $mailer)
+  {
+    $this->mailer = $mailer;
+  }
+
+  /**
    * Get the user's full name from their first name and surname
    * 
    * @return string The user's full name
@@ -24,5 +46,17 @@ class User
   public function getFullName()
   {
     return trim("$this->first_name $this->surname");
+  }
+
+  /**
+   * Send the user a message
+   * 
+   * @param string $message The message
+   * 
+   * @return boolean True if sent, false otherwise
+   */
+  public function notify($message)
+  {
+    return $this->mailer->sendMessage($this->email, $message);
   }
 }
