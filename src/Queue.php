b3776d53476a40eccf3ca5bc8ce00ffe740dbdbb
TomozQ <inf.4clover@gmail.com>
2024-01-31 22:45:37 +0900

    Exception Test

--- src/Queue.php
+++ src/Queue.php
@@ -2,10 +2,32 @@
 
 class Queue
 {
+  /**
+   * Makimum number of items in the queue
+   * @var integer
+   */
+  public const MAX_ITEMS = 5;
+  
+  /**
+   * Queue items
+   * @var array
+   */
   protected $items = [];
 
+  /**
+   * Add an item to the end of the queue
+   * 
+   * @param mixed $item The item
+   * 
+   * @throws QueueException if the number of items on the queue exceeds
+   *                        the MAX_ITEMS value
+   */
   public function push($item)
   {
+    if ($this->getCount() == static::MAX_ITEMS) {
+      throw new QueueException("Queue is full");
+    }
+    
     $this->items[] = $item;
   }
 
