e918b6d5a117b6e148acca4bbd73824e1611d1b0
TomozQ <inf.4clover@gmail.com>
2024-02-04 21:21:34 +0900

    mockry sample

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,11 +1,17 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
 
-class ExampleTest extends TestCase
+class ExampleTest extends MockeryTestCase
 {
-  public function testAddinngTwoPlusTwoResultsInFour()
-  {
-    $this->assertEquals(4, 2 + 2);
-  }
+  // public function testAddinngTwoPlusTwoResultsInFour()
+  // {
+  //   $this->assertEquals(4, 2 + 2);
+  // }
+
+  // public function tearDown(): void
+  // {
+  //   Mockery::close();
+  // }
 }
