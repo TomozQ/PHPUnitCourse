85b2ebc75d7df1ba0e2792d9a0df69472589be2a
TomozQ <inf.4clover@gmail.com>
2024-02-04 21:29:38 +0900

    mockery but not work

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,10 +5,10 @@ use Mockery\Adapter\Phpunit\MockeryTestCase;
 
 class ExampleTest extends MockeryTestCase
 {
-  // public function testAddinngTwoPlusTwoResultsInFour()
-  // {
-  //   $this->assertEquals(4, 2 + 2);
-  // }
+  public function testAddinngTwoPlusTwoResultsInFour()
+  {
+    $this->assertEquals(4, 2 + 2);
+  }
 
   // public function tearDown(): void
   // {
--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -1,9 +1,15 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use SebastianBergmann\Type\VoidType;
 
 class OrderTest extends TestCase
 {
+  public function tearDown(): void
+  {
+    Mockery::close();
+  }
+
   public function testOrderIsProcessed()
   {
     $gateway = $this->getMockBuilder('PaymentGateway')
@@ -20,4 +26,17 @@ class OrderTest extends TestCase
     
     $this->assertTrue($order->process());
   }
+
+  // public function testOrderIsProcessedUsingMockery()
+  // {
+  //   $gateway = Mockery::mock('PaymentGateway');
+  //   $gateway->shouldReceivce('charge')
+  //           ->once()
+  //           ->with(200)
+  //           ->andReturn(true);
+
+  //   $order = new Order($gateway);
+  //   $order->amount = 200;
+  //   $this->assertTrue($order->process());
+  // }
 }
